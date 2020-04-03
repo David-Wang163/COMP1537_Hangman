ea2a896cae8508c67f9bfa7155fc8ca3f46fc434
xaviernguyen07 <59535701+xaviernguyen07@users.noreply.github.com>
2020-04-03 00:43:18 -0700

    final commit

--- hangman.js
+++ hangman.js
@@ -8,7 +8,12 @@ createGuessWord();
 let guessWord = randomWord();
 let guessWordInUpperCase = guessWord.guessWord.toUpperCase();
 let playerGuess = hiddenGuessWord();
-// object constructor
+/**
+ * 
+ * @param {string} color: color of the button
+ * @param {string} letter: letter from A-Z thats appear on a button
+ * The object constructor for button
+ */
 function Button(color, letter) {
     this.letterBtn = document.createElement("button");
     this.letterBtn.style.backgroundColor = color;
@@ -20,10 +25,11 @@ function Button(color, letter) {
     }
     document.getElementById("btns").appendChild(this.letterBtn);
 }
+/**
+ * The function will generate 26 buttons, from button A-Z
+ */
 function createButtons() {
-    // all of the letters
     let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    // color of buttons
     let colorArr = ['#00CED1', '#20B2AA', '#008080', '#FFE4B5', '#FFDAB9', '#EEE8AA', '#F0E68C', '#BDB76B',
         '#DDA0DD', '#EE82EE', '#DA70D6', '#FF00FF', '#BA55D3', '#9370DB', '#663399', '#8A2BE2', '#9400D3',
         '#8B008B', '#800080', '#4B0082', '#6A5ACD', '#483D8B', '#7B68EE', '#ADFF2F', '#7FFF00', '#7CFC00']
@@ -33,7 +39,9 @@ function createButtons() {
         btnArr.push(btn);
     }
 }
-// create randomWord
+/**
+ * the function create 10 words with their definition using object constructor.
+ */
 function createGuessWord() {
     let wordList = ['committee', 'Tattoo', 'electricity', 'computer', 'InternetOfThings', 'OOP', 'teamwork', 'life', 'dog', 'cat'];
     let definition = ['a group of people appointed for a specific function, typically consisting of members of a larger group.',
@@ -52,16 +60,29 @@ function createGuessWord() {
         listOfWords.push(word);
     }
 }
+/**
+ * the function will pick a random word and its definition from the list.
+ */
 function randomWord() {
     let randomIndex = Math.floor(Math.random() * listOfWords.length);
     let word = listOfWords[randomIndex];
     return word;
 }
+/**
+ * 
+ * @param {string} word: a word from the populated list
+ * @param {string} definition: a definition comes with the word
+ * The object constructor for each word and its definition
+ */
 function GuessWordAndDefinition(word, definition) {
     this.guessWord = word;
     this.wordDefinition = definition;
 }
-// create hangman
+/**
+ * 
+ * @param {string} url: the source of image
+ * The object constructor for hangman 
+ */
 function MyHangman(url) {
     this.hangMan = document.createElement("img");
     this.hangMan.className = "hangMan";
@@ -71,7 +92,11 @@ function MyHangman(url) {
     this.changeStage(url);
     document.body.appendChild(this.hangMan);
 }
-// function to check if player guesses correct letter in randomWord
+/**
+ * 
+ * @param {string} buttonLetter
+ * The function will check if a letter from the button that user clicked exists in the guess word. 
+ */
 function checkLetter(buttonLetter) {
     let wordDict = checkNumOfOccurrences();
     if (guessWordInUpperCase.indexOf(buttonLetter) > -1) {
@@ -83,14 +108,17 @@ function checkLetter(buttonLetter) {
     }
     else {
         countClick += 1;
-        changeStageOfHangMan();
         Attempt -= 1;
         document.querySelector("#attempt").innerHTML = "Attempt: " + Attempt;
         score -= 1;
         document.querySelector("#score").innerHTML = "Score: " + score;
+        changeStageOfHangMan();
     }
 }
-// if players does not get the word correct
+/**
+ * The function will change the url when the user picked the letter
+ * that does not exist in the guess word.
+ */
 function changeStageOfHangMan() {
     if (countClick < 7) {
         myHangMan.changeStage("image/stage" + countClick + ".png");
@@ -100,7 +128,10 @@ function changeStageOfHangMan() {
         gameStatus()
     }
 }
-// check word frequency
+/**
+ * The function will calculate how many times each letter appears in the guess word
+ * Return an object with the key is each letter and the value is its frequency
+ */
 function checkNumOfOccurrences() {
     let wordFreqDict = {};
     for (let i = 0; i < guessWordInUpperCase.length; i++) {
@@ -111,21 +142,33 @@ function checkNumOfOccurrences() {
     }
     return wordFreqDict;
 }
-
+/**
+ * The function will be executed when the game ends
+ */
 function gameStatus() {
-    document.getElementById("messageToPlayer").textContent = "You Lose! the word was " + guessWordInUpperCase;
+    let playerName = prompt("Please enter your name");
+    document.getElementById("messageToPlayer").textContent = "GAME OVER!!!!!!" + playerName.charAt(0).toUpperCase() + playerName.slice(1) +
+        ", your score: " + score + ". The word was " + guessWordInUpperCase;
     for (let i = 0; i < btnArr.length; i++) {
         btnArr[i].letterBtn.disabled = true;
     }
 }
-
+/**
+ * The function will hide the guess word and display
+ * the number of underscores corresponding with the length of the guess word
+ */
 function hiddenGuessWord() {
     let hiddenWord = [];
     for (let i = 0; i < guessWordInUpperCase.length; i++)
         hiddenWord.push("_");
     return hiddenWord;
 }
-
+/**
+ * 
+ * @param {string} playerGuessLetter
+ * The function will overwrite the underscore with the correct letter
+ * when the player guesses correctly 
+ */
 function displayPlayerGuess(playerGuessLetter) {
     let h2Tag = document.querySelector("h2");
     let splitGuessWord = guessWordInUpperCase.split("");
@@ -137,23 +180,33 @@ function displayPlayerGuess(playerGuessLetter) {
     }
     h2Tag.innerHTML = playerGuess.join(" ");
     if (h2Tag.innerHTML === splitGuessWord.join(" ") && Attempt > 0) {
-        document.querySelector("#messageToPlayer").innerHTML = "Congratulations!!! You win";
+        document.querySelector("#messageToPlayer").innerHTML = "Congratulations!!! You win. Your score: " + score;
         for (let i = 0; i < btnArr.length; i++) {
             btnArr[i].letterBtn.disabled = true;
         }
     }
 }
+/**
+ * 
+ * @param {string} buttonLetter 
+ * The function will disable the button (button is not clickable)
+ * after the user guesses correct letter
+ */
 function disableButton(buttonLetter) {
     for (let i = 0; i < btnArr.length; i++) {
         if (btnArr[i].letterBtn.innerHTML === buttonLetter)
             btnArr[i].letterBtn.disabled = true;
     }
 }
-
+/**
+ * The function will be executed when the user click on restart button
+ */
 function restart() {
     window.location.reload();
 }
-
+/**
+ * The function will be executed when the user click on hint button
+ */
 function hint() {
     document.querySelector("#messageToPlayer").innerHTML = "Definition: " + guessWord.wordDefinition;
     document.querySelector("#hint").disabled = true;
